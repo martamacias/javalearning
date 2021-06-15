130d60ae4bcb617383b4d8cf61e99e1b06430a20
martamacias <program.martamacias@gmail.com>
2021-06-15 22:04:14 +0200

    tres en raya v0.1

--- curso/src/tres/TicTacToe.java
+++ curso/src/tres/TicTacToe.java
@@ -3,6 +3,8 @@ package curso.src.tres;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TicTacToe {
     public static void main(String[] args) {
@@ -19,15 +21,25 @@ class MarcoTicTac extends JFrame{
 
         LaminaTicTac lamina = new LaminaTicTac();
         add(lamina);
-        setSize(400, 400);
+        setSize(400, 450);
     }
 }
 
 // LAMINA
 class LaminaTicTac extends JPanel{
 
+    // donde va botón de reset
+    private JPanel laminaBotones = new JPanel();
+    private JButton reset = new JButton("RESET");
+    private Map<String, String> index = new HashMap<>();
+    // donde van los botones
     public LaminaTicTac(){
-        setLayout(new GridLayout(3,3,10,10));
+        setLayout(new BorderLayout());
+        add(laminaBotones);
+        reset.setFont(new Font("Ubuntu", 1, 20));
+        add(reset, BorderLayout.NORTH);
+
+        laminaBotones.setLayout(new GridLayout(3,3,10,10));
 
         //crear accion oyente y vincularla al boton
         PonerFicha marcarBoton = new PonerFicha();
@@ -35,10 +47,12 @@ class LaminaTicTac extends JPanel{
         // añadir botones
         for (int i = 1; i < 10; i++) {
             JButton boton = new JButton();
+            boton.setName(""+i);
             boton.setBackground(Color.WHITE);
             boton.addActionListener(marcarBoton);
             boton.setFont(new Font("Ubuntu", 1, 36));
-            add(boton);
+            laminaBotones.add(boton);
+            index.put(""+i, "null");
         }
     }
 
@@ -50,14 +64,21 @@ class LaminaTicTac extends JPanel{
         @Override
         public void actionPerformed(ActionEvent e) {
             JButton boton = (JButton) e.getSource();
-            if (count%2==0){
-                ficha = "X";
-            } else {
-                ficha = "O";
-            }
-            boton.setText(ficha);
+            String id = boton.getName();
+            
+            if (count%2==0){ ficha = "X";} 
+            else { ficha = "O"; }
             count ++;
+            boton.setText(ficha);
+
+            index.replace(id, "null", ficha);
+            if (!index.containsValue("null")) {
+                reset.setBackground(Color.RED);
+            }
         }
     }
 }
 
+// cuando acaba partida (de momento todos marcados)
+// no poder marcar botones marcados
+// quien gana / pierde + acaba partida
